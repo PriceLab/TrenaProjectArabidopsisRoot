b2dcf7dd55818320edfd90b698823e3800b4d7aa
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-07-19 14:29:35 -0700

    lapply solves the problem

--- explore/dataFrameRowExpansion/paulsSuggestion/rowExpansion.R
+++ explore/dataFrameRowExpansion/paulsSuggestion/rowExpansion.R
@@ -15,15 +15,15 @@ runTests <- function()
 obsolete.splitMultipleNamesIntoTheirOwnRows <- function(tbl)
 {
   stopifnot(nrow(tbl) == 1)    # we only handle data.frames with one row
-  
+
   if(!grepl(";", rownames(tbl))){
      return(tbl)  # just give it back to the caller
      }
 
   tokens <- strsplit(rownames(tbl), ";")[[1]]
   tbl[rep(seq_len(nrow(tbl)), each=length(tokens)),]
-  
-  
+
+
   if(length(tokens) == 2)
      tbl.result <- rbind(tbl, tbl)
 
@@ -36,8 +36,6 @@ obsolete.splitMultipleNamesIntoTheirOwnRows <- function(tbl)
   rownames(tbl.result) <- tokens
 
   return(tbl.result)
-  # your code to split the rowname, and duplicate the row values, building
-  # up new data.frame, of two or more rows,  will happen here
 
 } # obsolete.splitMultipleNamesIntoTheirOwnRows
 #----------------------------------------------------------------------------------------------------
@@ -47,21 +45,21 @@ splitMultipleNames <- function(tbl)
   tokens <- strsplit(rownames(tbl), ";")[[1]]
   tbl.result <- tbl[rep(seq_len(nrow(tbl)), each=length(tokens)),]
   rownames(tbl.result) <- tokens
-  
+
   return(tbl.result)
 
 } # splitMultipleNames
 #----------------------------------------------------------------------------------------------------
-
 splitMultiNamesWithMultiRows <- function(tbl.test)
 {
   stopifnot(nrow(tbl.test) > 1)
   orfNames <- strsplit(rownames(tbl.test), ";")[[10]] #how do I read 1-10 rather than just line 10?
   tbl.results <- tbl.test[rep(seq_len(nrow(tbl.test)), each=length(orfNames)),] # fix line 59 first, then come back to edit this
   rownames(tbl.results) <- orfNames #this should be fine once lines 59-60 runs correctly
-  
+
   return(tbl.results)
-}  #splitMultiNamesWithMultiRows
+
+}  # splitMultiNamesWithMultiRows
 #----------------------------------------------------------------------------------------------------
 test_obsoleteSplitMultipleNames <- function()
 {
@@ -80,37 +78,62 @@ test_obsoleteSplitMultipleNames <- function()
   tbl.fixed <- splitMultipleNames(tbl.2)
   checkEquals(nrow(tbl.fixed), 2)
   checkEquals(rownames(tbl.fixed),c("ATMG01250", "AT2G07697"))
-  
+
   tbl.3 <- tbl.1
   rownames(tbl.3) <- "aaa;bbb;ccc"
   tbl.fixed <- splitMultipleNames(tbl.3)
   checkEquals(nrow(tbl.fixed), 3)
   checkEquals(rownames(tbl.fixed),c("aaa", "bbb", "ccc"))
-  
+
   tbl.4 <- tbl.1
   rownames(tbl.4) <- "abc;def;ghi;jkl"
   tbl.fixed <- splitMultipleNames(tbl.4)
   checkEquals(nrow(tbl.fixed), 4)
   checkEquals(rownames(tbl.fixed),c("abc", "def", "ghi", "jkl"))
-  
+
 } # test_obsoleteSplitMultipleNames
 #----------------------------------------------------------------------------------------------------
-test_splitMultipleNames <- function() 
-{  
+test_splitMultipleNames <- function()
+{
   message(sprintf("--- test_splitMultipleNames"))   # announce what test you are running
-  
+
   tbl.5 <- tbl.test[10,]
   tbl.fixed <- splitMultipleNames(tbl.5)
   checkEquals(nrow(tbl.fixed), 2)
   checkEquals(rownames(tbl.fixed), c("ATMG01250","AT2G07697"))
+
 } #test_splitMultipleNames
 #----------------------------------------------------------------------------------------------------
 test_splitMultiNamesWithMultiRows <- function()
 {
   message(sprintf("---test_splitMultipleNames")) # announce what test you are running
-  
-  
+
+
 }  #test_splitMultiNamesWithMultiRows
 #----------------------------------------------------------------------------------------------------
+demo.doAllRows <- function(tbl)
+{
+   tbl <- get(load("10by10rootTable.RData"))
+   x <- lapply(1:nrow(tbl), function(i) splitMultipleNames(tbl[i,]))
+   tbl.combined <- do.call(rbind, x)
+   return(tbl.combined)
+
+} # demo.doAllRows
+#------------------------------------------------------------------------------------------------------------------------
+test_demo.doAllRows <- function()
+{
+   message(sprintf("--- test_demo.doAllRows"))
+   tbl.expanded <- demo.doAllRows(tbl.test)
+
+   checkTrue(nrow(tbl.expanded) > nrow(tbl.test))
+
+   multiple.orf.names.orig <- length(grep(";", rownames(tbl.test)))
+   checkEquals(multiple.orf.names.orig, 3)
+
+   multiple.orf.names.expanded <- length(grep(";", rownames(tbl.expanded)))
+   checkEquals(multiple.orf.names.expanded, 0)
+
+} # test_demo.doAllRows
+#------------------------------------------------------------------------------------------------------------------------
 if(!interactive())
    runTests()
