a129709833ddb8bb53c123c4535ae2bb072584aa
thardy615 <thardy1@scmail.spelman.edu>
2019-07-25 16:13:33 -0700

    test my function out

--- explore/importRootExpressionData/identifyMultiOrfNames.R
+++ explore/importRootExpressionData/identifyMultiOrfNames.R
@@ -1,19 +1,36 @@
 library(RUnit)
 #----------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+   test_identifyMultiOrfNames()
+   
+} # runTests
+#----------------------------------------------------------------------------------------------------
 # create function identifyMultiOrfNames
 identifyMultiOrfNames <- function(tbl)
 {
-  tbl <- read.table("roots.tsv", sep = "\t", header= TRUE, as.is=TRUE, nrow=400)
-  multiOrfNames <- grep(";", tbl$Alias, value =TRUE)
-  singleOrfNames <- unlist(strsplit(multiOrfNames, ";"))
+  singleOrfNames <- unlist(strsplit(grep(";", tbl, value = TRUE), ";"))
+  return(singleOrfNames)
 }
 #---------------------------------------------------------------------------------------------------
 test_identifyMultiOrfNames <- function()
 {
    message(sprintf("--- test_identifyMultiOrfNames"))
-   result <- idenitfyMultiOrfNames("abcd;efgh")
+   
+   result <- identifyMultiOrfNames("abcd;efgh")
    checkEquals(result, c("abcd", "efgh"))
-
+   
+   result.2 <- identifyMultiOrfNames("abcd;efgh;ijkl")
+   checkEquals(result.2, c("abcd", "efgh", "ijkl"))
+   
+   result.3 <- identifyMultiOrfNames("abcd;efgh;ijkl;mnop")
+   checkEquals(result.3, c("abcd", "efgh", "ijkl", "mnop"))
+   
+   result.4 <- identifyMultiOrfNames("abcd;efgh;ijkl;mnop;qrst")
+   checkEquals(result.4, c("abcd", "efgh", "ijkl", "mnop", "qrst"))
 
 } # test_identifyMultiOrfNames
+#----------------------------------------------------------------------------------------------------
+if(!interactive())
+   runTests()
 #---------------------------------------------------------------------------------------------------
