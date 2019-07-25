a1a7ddfbaf3c981acec29da5396d63bd39ece073
thardy615 <thardy1@scmail.spelman.edu>
2019-07-24 17:20:22 -0700

    added addFour test

--- explore/importRootExpressionData/createAddFour.R
+++ explore/importRootExpressionData/createAddFour.R
@@ -6,3 +6,24 @@ addFour <- function(d)
   return(sum)
   
 } #addFour()
+
+#----------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+  test_addFour()
+  
+} # runTests
+#----------------------------------------------------------------------------------------------------
+test_addFour <- function()
+{
+  message(sprintf("--- test_addFour"))
+  
+  sum <- addFour(d)
+  checkTrue(sum > d)
+  checkEquals(sum, d + 4)
+  checkTrue(class(sum)== "numeric")
+  
+} # test_createTestMatrix
+#----------------------------------------------------------------------------------------------------
+if(!interactive())
+  runTests()
