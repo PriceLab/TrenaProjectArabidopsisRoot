4ac103f6ccf8eab407603e49ad9f948dc041b43b
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-07-25 15:42:41 -0700

    added test

--- explore/importRootExpressionData/identifyMultiOrfNames.R
+++ explore/importRootExpressionData/identifyMultiOrfNames.R
@@ -6,4 +6,14 @@ identifyMultiOrfNames <- function(tbl)
   tbl <- read.table("roots.tsv", sep = "\t", header= TRUE, as.is=TRUE, nrow=400)
   multiOrfNames <- grep(";", tbl$Alias, value =TRUE)
   singleOrfNames <- unlist(strsplit(multiOrfNames, ";"))
-}  
+}
+#---------------------------------------------------------------------------------------------------
+test_identifyMultiOrfNames <- function()
+{
+   message(sprintf("--- test_identifyMultiOrfNames"))
+   result <- idenitfyMultiOrfNames("abcd;efgh")
+   checkEquals(result, c("abcd", "efgh"))
+
+
+} # test_identifyMultiOrfNames
+#---------------------------------------------------------------------------------------------------
