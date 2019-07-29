2efe2dbceb373fcd82aee65c3fc1bb9c736427af
Camille Price <camille.anna.price@gmail.com>
2019-07-29 11:07:12 -0700

    added test for multiple predictors model

--- explore/camille/test_mtcarsR.R
+++ explore/camille/test_mtcarsR.R
@@ -1,22 +1,25 @@
 library(RUnit)
 source("mtcarsR.R")
 #-------------------------------------------------------------------------------
-run_tests <- function()
+runTests <- function()
 {
-  test_one_predictor()
-  test_two_predictors()
+  test_one_predictor_rsquared()
+  test_two_predictors_rsquared()
+  test_multiple_predictors_model()
+  #test_two_predictors_residuals
 }#testing all
 #----------------------------------------------------------------------------------------------------
-test_one_predictor <- function()
+test_one_predictor_rsquared <- function()
 {
   message(sprintf("--- test_one_predictor"))
   model.cyl1 <- lm(formula = mtcars$mpg ~ mtcars$cyl)
   checkEquals(one_predictor_rsquared("mpg", "cyl"),summary(model.cyl1)$r.squared)
   model.qsec1 <- lm(formula = mtcars$mpg ~ mtcars$qsec)
   checkEquals(one_predictor_rsquared("mpg", "qsec"),summary(model.qsec1)$r.squared)
+  
 }#test_one_predictor
 #---------------------------------------------------------------------------
-test_two_predictors <- function()
+test_two_predictors_rsquared <- function()
 {
   message(sprintf("--- test_two_predictors"))
   model.cylwt <- lm(formula = mtcars$mpg ~ mtcars$cyl + mtcars$wt)
@@ -25,5 +28,18 @@ test_two_predictors <- function()
   checkEquals(two_predictors_rsquared("mpg","hp", "am"),summary(model.hpam)$r.squared)
 }#test_two_predictors
 #---------------------------------------------------------------------------
+test_multiple_predictors_model <- function()
+{
+  message(sprintf("--- test_multiple_predictors_model"))
+  model.amwt <- lm(formula = mtcars$mpg ~ mtcars$am + mtcars$wt)
+  checkEquals(multiple_predictors_model(mtcars, "mpg", c("am","wt")),summary(model.amwt)$r.squared)
+  model.cylhpvs <- lm(formula = mtcars$mpg ~ mtcars$cyl + mtcars$hp + mtcars$vs)
+  checkEquals(multiple_predictors_model(mtcars, "mpg", c("cyl","hp", "vs")),summary(model.cylhpvs)$r.squared)
+}#test_multiple_predictors_model
+#---------------------------------------------------------------------------
+
+
+#test_two_predictors_residuals
+#---------------------------------------------------------------------------
 if(!interactive())
-  run_tests()
+  runTests()
